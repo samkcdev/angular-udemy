7b7347e6c08a16ebc009fccd85a5318fc4d52d84
Sam Cherian <samnp87@gmail.com>
2018-05-15 23:24:19 +0530

    Learned to create component manually and also with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {WarningComponent} from './warning/warning-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// the component decorator that enhances the class
+@Component ({
+    selector : 'app-server',
+    templateUrl : './server.component.html'
+})
+
+export class ServerComponent{
+
+}
